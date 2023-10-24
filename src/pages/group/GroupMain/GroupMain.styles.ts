2237c7aecd7a3285624c2d829a8d81043ff593bc
geniee1220 <geniee1220@gmail.com>
2023-10-24 20:26:29 +0900

    Feat : 그룹 페이지 생성

---
+++ src/pages/group/GroupMain/GroupMain.styles.ts
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const GroupMainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  background-color: ${(props) => props.theme.color.secondary};
+`;
---
+++ src/pages/group/GroupMain/index.tsx
@@ -0,0 +1,7 @@
+import * as S from './GroupMain.styles';
+
+function GroupMain() {
+  return <S.GroupMainContainer>그룹 메인 페이지입니다.</S.GroupMainContainer>;
+}
+
+export default GroupMain;
