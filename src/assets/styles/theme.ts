241b2b660c558d7724313e81332088ec90db11b1
geniee1220 <geniee1220@gmail.com>
2023-10-24 19:50:54 +0900

    Refactor : 전역 변수에 타입 지정

--- src/assets/styles/theme.ts
+++ src/assets/styles/theme.ts
@@ -1,3 +1,5 @@
+import { Theme } from '@emotion/react';
+
 const layoutComponent = {
   header_height: 68,
   footer_height: 94,
@@ -7,7 +9,7 @@ const color = {
   primary: '#3182f6',
 };
 
-const theme = {
+const theme: Theme = {
   layoutComponent,
   color,
 };
