abd4de3a037f310daf10d55c15394f6994b0cc1b
jiyujin <yujin1679@naver.com>
2023-10-24 23:48:01 +0900

    Feat: root 라우트에 Home 컴포넌트 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
 import GroupMain from './pages/group/GroupMain';
+import Home from './pages/Home';
 
 const router = createBrowserRouter([
   {
@@ -8,6 +9,7 @@ const router = createBrowserRouter([
     element: <App />,
     errorElement: <div>404 Not Found</div>,
     children: [
+      { index: true, path: '/', element: <Home /> },
       {
         path: '/group',
         element: <GroupMain />,
