f78b19c097cb7b9d7e5f8f1677771947daa3695a
jiyujin <yujin1679@naver.com>
2023-10-24 23:48:44 +0900

    Feat: Home 컴포넌트 생성

---
+++ src/pages/Home/Home.styles.ts
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+export const HomeMainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+`;
+
+export const Banner = styled.div`
+  width: 100%;
+  height: 313px;
+  margin-top: 50px;
+  border-radius: 4px;
+  background-color: #3e837e;
+`;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './Home.styles';
+
+function GroupMain() {
+  return (
+    <S.HomeMainContainer>
+      <S.Banner />
+    </S.HomeMainContainer>
+  );
+}
+
+export default GroupMain;
