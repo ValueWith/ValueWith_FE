bdc92a896acac06a25cc134b5bf050eb2c9bbdf7
jiyujin <yujin1679@naver.com>
2023-10-25 01:02:22 +0900

    Design: Home 컴포넌트에 Banner 레이아웃 추가

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,6 +4,10 @@ function GroupMain() {
   return (
     <S.HomeMainContainer>
       <S.Banner />
+      <S.TitleContainer>
+        <S.Title>카테고리 이름</S.Title>
+        <S.Link>더 많은 일정 확인하기 &gt;</S.Link>
+      </S.TitleContainer>
     </S.HomeMainContainer>
   );
 }
