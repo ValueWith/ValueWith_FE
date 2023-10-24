dfd030d7088408679974ed43a9c9e413be96d3c0
geniee1220 <geniee1220@gmail.com>
2023-10-24 20:25:59 +0900

    Feat : Footer 컴포넌트 생성

---
+++ src/components/Footer/Footer.styles.ts
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled';
+
+export const FooterContainer = styled.div`
+  width: 100%;
+  height: ${(props) => props.theme.layoutComponent.footer_height}px;
+`;
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,7 @@
+import * as S from './Footer.styles';
+
+function Footer() {
+  return <S.FooterContainer>푸터 영역입니다.</S.FooterContainer>;
+}
+
+export default Footer;
