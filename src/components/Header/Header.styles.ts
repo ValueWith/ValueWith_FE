9f1af680ca8c4158d7a14808bd0f28679a6b3a95
geniee1220 <geniee1220@gmail.com>
2023-10-26 21:56:29 +0900

    Feat : Header 컴포넌트 생성

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -1,6 +1,50 @@
 import styled from '@emotion/styled';
+import theme from '@assets/styles/theme';
 
-export const HeaderContainer = styled.div`
+export const HeaderContainer = styled.header`
   width: 100%;
-  height: ${(props) => props.theme.layoutComponent.header_height}px;
+  height: ${theme.layoutComponent.header_height}px;
+  border-bottom: 1px solid ${theme.color.lightgray};
 `;
+
+export const HeaderInner = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  max-width: calc(1080px + 32px);
+  padding: 0 16px;
+  height: 100%;
+  margin: 0 auto;
+`;
+
+export const HeaderLogo = styled.div`
+  cursor: pointer;
+`;
+
+export const HeaderMenu = styled.nav`
+  display: inline-flex;
+  align-items: center;
+  margin-left: 68px;
+
+  .header__menu-list {
+    display: inline-flex;
+    align-items: center;
+    height: 100%;
+  }
+`;
+
+export const HeaderMenuItem = styled.li`
+  display: inline-flex;
+  height: 100%;
+  margin-right: 35px;
+  font-size: 15px;
+  font-weight: 500;
+  color: ${theme.color.fontgray};
+  cursor: pointer;
+
+  &.active {
+    color: ${theme.color.primary};
+  }
+`;
+
+export const HeaderFunction = styled.div``;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,51 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+
+// constants
+import { PAGE_LINK } from '@/constants/pagelink';
+
+// styles
 import * as S from './Header.styles';
+import theme from '@/assets/styles/theme';
+import Logo from '@assets/TweaverLogo.svg?react';
+import { useEffect, useState } from 'react';
 
 function Header() {
-  return <S.HeaderContainer>헤더 컴포넌트입니다</S.HeaderContainer>;
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const [currentCategory, setCurrentCategory] = useState<string>('');
+
+  useEffect(() => {
+    setCurrentCategory(location.pathname);
+  }, [location]);
+
+  return (
+    <S.HeaderContainer>
+      <S.HeaderInner>
+        <S.HeaderLogo>
+          <Logo
+            color={`${theme.color.primary}`}
+            onClick={() => navigate('/')}
+          />
+        </S.HeaderLogo>
+        <S.HeaderMenu>
+          <ul className="header__menu-list">
+            {PAGE_LINK.map((page, index) => {
+              return (
+                <S.HeaderMenuItem
+                  key={index}
+                  className={page.path === currentCategory ? 'active' : ''}
+                  onClick={() => navigate(page.path)}
+                >
+                  {page.name}
+                </S.HeaderMenuItem>
+              );
+            })}
+          </ul>
+        </S.HeaderMenu>
+      </S.HeaderInner>
+    </S.HeaderContainer>
+  );
 }
 
 export default Header;
