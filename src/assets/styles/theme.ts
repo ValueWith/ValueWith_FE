ad3ed65246fff3f84d8e22860bf90407b8f63c7c
geniee1220 <geniee1220@gmail.com>
2023-10-27 14:30:10 +0900

    Design : 디자인 시스템에 gray, red 색상 추가

--- src/assets/styles/theme.ts
+++ src/assets/styles/theme.ts
@@ -6,9 +6,19 @@ const layoutComponent = {
 };
 
 const color = {
+  white: '#ffffff',
   primary: '#3182f6',
   secondary: '#d0e9ff',
-  lightgray: '#eaeaea',
+  gray100: '#e8e8e8',
+  gray200: '#eaeaea',
+  gray300: '#bdbdbd',
+  gray400: '#b2b2b2',
+  gray500: '#b9b9b9',
+  gray600: '#9b9b9b',
+  gray700: '#828282',
+  red100: '#f8796b',
+  red200: '#f26757',
+  red300: '#ef4A37',
   fontgray: '#595F63',
 };
 
