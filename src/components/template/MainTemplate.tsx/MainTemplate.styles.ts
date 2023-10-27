efd2b4323118e3f0322c762ca8400c6c058ab9b6
geniee1220 <geniee1220@gmail.com>
2023-10-27 15:47:23 +0900

    Design : 시맨틱 태그로 변경하고 유저 페이지(로그인, 회원가입) 추가 스타일 지정

--- src/components/template/MainTemplate.tsx/MainTemplate.styles.ts
+++ src/components/template/MainTemplate.tsx/MainTemplate.styles.ts
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 import theme from '@assets/styles/theme';
 
-export const TemplateContainer = styled.div`
+export const TemplateContainer = styled.main`
   width: 100%;
   min-height: ${`calc(100% - ${theme.layoutComponent.header_height}px - ${theme.layoutComponent.footer_height}px)`};
 `;
@@ -11,4 +11,8 @@ export const TemplateInner = styled.div`
   max-width: calc(1080px + 32px);
   padding: 0 16px;
   margin: 0 auto;
+
+  &.user {
+    max-width: calc(450px + 32px);
+  }
 `;
--- src/components/template/MainTemplate.tsx/index.tsx
+++ src/components/template/MainTemplate.tsx/index.tsx
@@ -1,13 +1,23 @@
+import { useLocation } from 'react-router-dom';
 import * as S from './MainTemplate.styles';
+import { useEffect, useState } from 'react';
 
 interface MainTemplateProps {
   children: React.ReactNode;
 }
 
 function MainTemplate({ children }: MainTemplateProps) {
+  const { pathname } = useLocation();
+
+  const [pageStyle, setPageStyle] = useState('');
+
+  useEffect(() => {
+    setPageStyle(pathname === '/login' || pathname === '/signup' ? 'user' : '');
+  }, [pathname]);
+
   return (
     <S.TemplateContainer>
-      <S.TemplateInner>{children}</S.TemplateInner>
+      <S.TemplateInner className={pageStyle}>{children}</S.TemplateInner>
     </S.TemplateContainer>
   );
 }
