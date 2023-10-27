86ec831a0fd05daff75cb645fcf0218181198a23
geniee1220 <geniee1220@gmail.com>
2023-10-27 18:02:54 +0900

    Feat : Input(Textarea) 컴포넌트 생성

---
+++ src/components/Input/Input.styles.ts
@@ -0,0 +1,58 @@
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+import theme from '@assets/styles/theme';
+
+export const commonInputStyle = css`
+  width: 100%;
+  height: 42px;
+  border: 0;
+  font: inherit;
+  font-size: 14px;
+  border-radius: 4px;
+  border: 1px solid ${theme.color.gray200};
+
+  &::placeholder {
+    color: rgba(128, 128, 128, 0.9);
+  }
+
+  &:focus-within {
+    border: 1px solid ${theme.color.primary};
+  }
+
+  &.error {
+    border: 1px solid ${theme.color.red300};
+  }
+`;
+
+export const InputContainer = styled.div`
+  margin-bottom: 24px;
+`;
+
+export const InputLabel = styled.label`
+  display: block;
+  margin-bottom: 4px;
+`;
+
+export const Input = styled.input`
+  ${commonInputStyle}
+  padding:0 16px;
+`;
+
+export const Textarea = styled.textarea`
+  ${commonInputStyle}
+  padding: 10px;
+  min-height: 158px;
+  resize: none;
+  overflow-y: scroll;
+  overflow-y: overlay;
+
+  &::placeholder {
+    white-space: pre-wrap;
+  }
+`;
+
+export const InputErrorMessage = styled.p`
+  margin-top: 4px;
+  font-size: 14px;
+  color: ${theme.color.red300};
+`;
---
+++ src/components/Input/index.tsx
@@ -0,0 +1,70 @@
+import { forwardRef } from 'react';
+import * as S from './Input.styles';
+
+interface InputProps {
+  name: string;
+  inputType: 'textarea' | 'input';
+  type?: string; // text:default, password - input 타입에서 필수로 받아야 하는 값
+  label?: string; // input에 라벨 표시해야 하는 경우 전달
+  placeholder?: string; // input에 placeholder 표시해야 하는 경우 전달
+  readOnly?: boolean;
+  errors?: any; // errors,onChange는 useForm 에서 관리,  직접 Props로 넘겨주지 않음
+  onChange?: (
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLTextAreaElement>
+  ) => void;
+}
+
+function Input(
+  {
+    type = 'text',
+    inputType,
+    name,
+    label,
+    placeholder,
+    readOnly,
+    onChange,
+    errors,
+  }: InputProps,
+  ref: React.Ref<HTMLInputElement | HTMLTextAreaElement>
+) {
+  // 에러 메시지가 있는 경우 해당 에러 메시지를 errorKEY에 할당
+  const errorKEY = errors?.[name as string]?.message as string;
+
+  const inputCommonProps = {
+    id: name,
+    name: name,
+    onChange: onChange,
+    placeholder: placeholder,
+    autoComplete: 'off',
+    readOnly: readOnly,
+  };
+
+  return (
+    <S.InputContainer>
+      {label && <S.InputLabel htmlFor={name}>{label}</S.InputLabel>}
+      <div>
+        {inputType === 'textarea' ? (
+          <S.Textarea
+            className={errorKEY && 'error'}
+            spellCheck="false"
+            ref={ref as React.Ref<HTMLTextAreaElement>}
+            {...inputCommonProps}
+          />
+        ) : (
+          <S.Input
+            type={type}
+            className={errorKEY && 'error'}
+            spellCheck="false"
+            ref={ref as React.Ref<HTMLInputElement>}
+            {...inputCommonProps}
+          />
+        )}
+      </div>
+      {errorKEY && <S.InputErrorMessage>{errorKEY}</S.InputErrorMessage>}
+    </S.InputContainer>
+  );
+}
+
+export default forwardRef(Input);
