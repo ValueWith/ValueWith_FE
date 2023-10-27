2bb9d4fb2cc95346f41238eb9c22fe3214c3d44d
geniee1220 <geniee1220@gmail.com>
2023-10-27 14:30:18 +0900

    Feat : Button 컴포넌트 생성

---
+++ src/components/Button/Button.styles.ts
@@ -0,0 +1,89 @@
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+
+import theme from '@/assets/styles/theme';
+import { ButtonCSSProps } from '.';
+
+const handleButtonType = (props: ButtonCSSProps) => {
+  switch (props.styleType) {
+    case 'solid':
+      return css`
+        background-color: ${theme.color.primary};
+        color: ${theme.color.white};
+        border: 1px solid ${theme.color.primary};
+      `;
+    case 'outline':
+      return css`
+        background-color: ${theme.color.white};
+        color: ${theme.color.primary};
+        border: 1px solid ${theme.color.primary};
+      `;
+    case 'disabled':
+      return css`
+        background-color: ${theme.color.gray100};
+        border: 1px solid ${theme.color.gray500};
+        pointer-events: none;
+        cursor: default;
+      `;
+    case 'warning':
+      return css`
+        background-color: ${theme.color.red200};
+        color: ${theme.color.white};
+        border: 1px solid ${theme.color.red200};
+      `;
+    case 'text':
+      return css`
+        color: ${theme.color.primary};
+      `;
+    case 'basic':
+      return css`
+        background-color: ${theme.color.white};
+        border: 1px solid ${theme.color.gray100};
+        color: ${theme.color.fontgray};
+      `;
+  }
+};
+
+const handleButtonSize = (props: ButtonCSSProps) => {
+  switch (props.size) {
+    case 'sm':
+      return css`
+        min-width: 82px;
+        height: 36px;
+        font-size: 14px;
+        font-size: 1.4rem;
+      `;
+    case 'md':
+      return css`
+        min-width: 96px;
+        height: 42px;
+        font-size: 16px;
+        font-size: 1.6rem;
+      `;
+    case 'lg':
+      return css`
+        min-width: 278px;
+        height: 56px;
+        font-size: 18px;
+        font-size: 1.8rem;
+      `;
+    default:
+      return '';
+  }
+};
+
+export const Button = styled.button<ButtonCSSProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  max-width: ${(props) => (props.fullWidth ? '100%' : 'fit-content')};
+  padding: 0 16px;
+  border-radius: 4px;
+  border: 1px solid transparent;
+  white-space: nowrap;
+  appearance: none;
+
+  ${(props) => handleButtonType(props)}
+  ${(props) => handleButtonSize(props)}
+`;
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,41 @@
+import * as S from './Button.styles';
+
+export interface ButtonCSSProps {
+  size?: 'sm' | 'md' | 'lg'; // 버튼 사이즈, default: md
+  styleType?: 'basic' | 'solid' | 'outline' | 'disabled' | 'warning' | 'text'; // 버튼 디자인 타입, default: solid
+  fullWidth?: boolean; // 버튼 가로 너비 100%, default: false
+}
+
+interface ButtonProps extends ButtonCSSProps {
+  type?: 'button' | 'submit' | 'reset'; // 버튼 타입, default: submit
+  children: React.ReactNode;
+  style?: React.CSSProperties;
+  onClickHandler?: (e?: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+function Button({
+  children,
+  style,
+  type = 'submit',
+  size = 'md',
+  styleType = 'solid',
+  fullWidth = false,
+  onClickHandler,
+}: ButtonProps) {
+  return (
+    <S.Button
+      type={type}
+      size={size}
+      styleType={styleType}
+      fullWidth={fullWidth}
+      style={{
+        ...style,
+      }}
+      onClick={onClickHandler}
+    >
+      {children}
+    </S.Button>
+  );
+}
+
+export default Button;
