f7dac42556ad8f4d8ab7d12ac1f870f094f2aa22
geniee1220 <geniee1220@gmail.com>
2023-10-27 14:51:28 +0900

    Design : Footer 컴포넌트에 스타일 추가, 컨텍스트 추가

--- src/components/Footer/Footer.styles.ts
+++ src/components/Footer/Footer.styles.ts
@@ -1,6 +1,25 @@
 import styled from '@emotion/styled';
+import theme from '@assets/styles/theme';
 
-export const FooterContainer = styled.div`
+export const FooterContainer = styled.footer`
   width: 100%;
   height: ${(props) => props.theme.layoutComponent.footer_height}px;
+  border-top: 1px solid ${theme.color.gray200};
+`;
+
+export const FooterInner = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  max-width: calc(1080px + 32px);
+  padding: 0 16px;
+  height: 100%;
+  margin: 0 auto;
+`;
+
+export const FooterContext = styled.p`
+  font-size: 14px;
+  font-weight: 500;
+  color: #767d82;
 `;
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,15 @@
 import * as S from './Footer.styles';
 
 function Footer() {
-  return <S.FooterContainer>푸터 영역입니다.</S.FooterContainer>;
+  return (
+    <S.FooterContainer>
+      <S.FooterInner>
+        <S.FooterContext>
+          Copyright by Project ValueWith. 2023 All rights reserved.
+        </S.FooterContext>
+      </S.FooterInner>
+    </S.FooterContainer>
+  );
 }
 
 export default Footer;
