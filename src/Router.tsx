f6f9d4220f980eee8a42620f7011972b1cf06b02
geniee1220 <geniee1220@gmail.com>
2023-10-28 11:44:53 +0900

    Chore : 로그인 및 회원가입 페이지 라우터 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
 import GroupMain from './pages/group/GroupMain';
+import Signup from './pages/user/Signup';
 
 const router = createBrowserRouter([
   {
@@ -8,6 +9,16 @@ const router = createBrowserRouter([
     element: <App />,
     errorElement: <div>404 Not Found</div>,
     children: [
+      {
+        // 로그인 페이지
+        path: '/login',
+        element: <div>로그인 페이지</div>,
+      },
+      {
+        // 회원가입 페이지
+        path: '/signup',
+        element: <Signup />,
+      },
       {
         path: '/group',
         element: <GroupMain />,
