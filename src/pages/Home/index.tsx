1596c382b4d9050bafbc51da15f8bab5d3f90156
jiyujin <yujin1679@naver.com>
2023-11-01 13:50:18 +0900

    Feat: 버튼 클릭 시 group 페이지로 이동

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+
 import TripList from '@/components/TripList';
+
 import * as S from './Home.styles';
 
 function Home() {
+  const navigate = useNavigate();
+
   return (
     <S.HomeMainContainer>
       <S.Banner />
       <S.TitleContainer>
         <S.Title>카테고리 이름</S.Title>
-        <S.Link>더 많은 일정 확인하기 &gt;</S.Link>
+        <S.Link onClick={() => navigate('/group')}>
+          더 많은 일정 확인하기 &gt;
+        </S.Link>
       </S.TitleContainer>
-      <TripList pageName={'Home'}/>
+      <TripList pageName={'Home'} />
     </S.HomeMainContainer>
   );
 }
