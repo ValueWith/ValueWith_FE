b2b324778ccea7c5f462f6fc84ba6533f1a71265
jiyujin <yujin1679@naver.com>
2023-11-03 18:22:13 +0900

    Feat: Filter 옵션 상수 파일 분리

--- src/components/GroupFilter/index.tsx
+++ src/components/GroupFilter/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+import {
+  areaOptions,
+  sortingOptions,
+  statusOptions,
+} from '@/constants/filterOption';
+
 import RadioGroup from '../RadioGroup';
 
 import * as S from './GroupFilter.styles';
@@ -23,36 +29,6 @@ function GroupFilter({
   area,
   setArea,
 }: GroupFilterProps) {
-  const recruitemStatusOptions = [
-    { label: '전체보기', value: 'all' },
-    { label: '모집중', value: 'recruiting' },
-  ];
-
-  const sortingOptions = [
-    { label: '최신순', value: 'latest' },
-    { label: '마감순', value: 'deadline' },
-  ];
-
-  const areaOptions = [
-    { label: '전체', value: 'all' },
-    { label: '강원', value: 'gangwon' },
-    { label: '경기', value: 'gyeonggi' },
-    { label: '경남', value: 'gyeongnam' },
-    { label: '경북', value: 'gyeongbuk' },
-    { label: '대구', value: 'daegu' },
-    { label: '대전', value: 'daejeon' },
-    { label: '부산', value: 'busan' },
-    { label: '서울', value: 'seoul' },
-    { label: '세종', value: 'sejong' },
-    { label: '울산', value: 'ulsan' },
-    { label: '인천', value: 'incheon' },
-    { label: '전남', value: 'jeonnam' },
-    { label: '전북', value: 'jeonbuk' },
-    { label: '제주', value: 'jeju' },
-    { label: '충남', value: 'chungcheongnam' },
-    { label: '충북', value: 'chungcheongbuk' },
-  ];
-
   const handleRecruitmentStatusChange = (value: string) => {
     setRecruitmentStatus(value);
   };
@@ -71,7 +47,7 @@ function GroupFilter({
         <>
           <S.FilterTitle>모집현황</S.FilterTitle>
           <RadioGroup
-            options={recruitemStatusOptions}
+            options={statusOptions}
             selectedValue={recruitmentStatus}
             styleType='radio'
             onChange={handleRecruitmentStatusChange}
