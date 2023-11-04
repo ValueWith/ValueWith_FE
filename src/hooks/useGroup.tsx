fa1026dd5b21439e75a737a8379cc4a8db87758d
jiyujin <yujin1679@naver.com>
2023-11-04 14:26:41 +0900

    Refactor: useQuery의 queryKey에 params 전달

--- src/hooks/useGroup.tsx
+++ src/hooks/useGroup.tsx
@@ -2,7 +2,7 @@ import { useQuery } from 'react-query';
 import { GroupListItem, GroupListParams, fetchGroupList } from '@/apis/group';
 
 const useGroupDataFetching = (params: GroupListParams) => {
-  return useQuery<GroupListItem, Error>('groupData', () =>
+  return useQuery<GroupListItem, Error>(['groupData', params], () =>
     fetchGroupList(params)
   );
 };
