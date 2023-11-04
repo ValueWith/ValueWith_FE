22c28f1800e11335c5afa5780a4f3041d87577e9
geniee1220 <geniee1220@gmail.com>
2023-11-04 20:41:05 +0900

    Feat : 지도에 geoLocation으로 사용자의 현재 위치값을 기본 값으로 지정

--- src/components/group/recruit/KakaoMap/index.tsx
+++ src/components/group/recruit/KakaoMap/index.tsx
@@ -1,11 +1,41 @@
+import { useEffect, useState } from 'react';
 import { Map } from 'react-kakao-maps-sdk';
 
 function KakaoMap() {
+  const [mapOptions, setMapOptions] = useState({
+    center: { lat: 33.450701, lng: 126.570667 },
+    level: 3,
+    isPanto: true,
+    zoomable: false,
+  });
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+
+          setMapOptions({
+            ...mapOptions,
+            center: { lat: latitude, lng: longitude },
+          });
+        },
+        (error) => {
+          console.error(error);
+        },
+        {
+          enableHighAccuracy: true,
+          maximumAge: 0, // 캐시된 위치 정보를 사용하지 않음
+          timeout: Infinity, // 위치 정보를 가져오는데 걸리는 시간 제한 없음
+        }
+      );
+    }
+  }, [mapOptions, setMapOptions]);
+
   return (
     <Map
-      center={{ lat: 33.5563, lng: 126.79581 }} // 지도의 중심 좌표
       style={{ width: '100%', height: '100%' }} // 지도 크기
-      level={3} // 지도 확대 레벨
+      {...mapOptions}
     ></Map>
   );
 }
