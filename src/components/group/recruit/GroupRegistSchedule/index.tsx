774abe6fd404aa2a088d0751e73c3614f7da27a4
geniee1220 <geniee1220@gmail.com>
2023-11-06 20:17:00 +0900

    Feat : 장소 추천 버튼 클릭 시 사이드바 토글 로직 추가

--- src/components/group/recruit/GroupRegistSchedule/index.tsx
+++ src/components/group/recruit/GroupRegistSchedule/index.tsx
@@ -4,6 +4,7 @@ import NestedSidebar from '../NestedSidebar';
 
 import * as S from '@components/group/recruit/GroupRegist.styles';
 import SearchBar from '@/components/SearchBar';
+import Button from '@/components/Button';
 
 function GroupRegistSchedule() {
   const [searchTerm, setSearchTerm] = useState<string>('');
@@ -13,24 +14,43 @@ function GroupRegistSchedule() {
     type: 'search', // 'suggest' | 'search'
   });
 
-  const handleSearchTerm = (term: string, type: 'suggest' | 'search') => {
+  const handleSearchTerm = (
+    term: string,
+    type: 'suggest' | 'search',
+    toggled?: boolean
+  ) => {
     setSearchTerm(term);
     setIsNestedSidebar({
       type: type,
-      status: true,
+      status: toggled ? !isNestedSidebar.status : true,
     });
   };
 
   return (
     <S.GroupRegistContainer>
       {/* 검색창 */}
-      <SearchBar
-        onSearchTermChange={(term) => handleSearchTerm(term, 'search')}
-      />
+      <div className="registGroup">
+        <SearchBar
+          onSearchTermChange={(term) => handleSearchTerm(term, 'search')}
+        />
+      </div>
 
-      <span onClick={() => handleSearchTerm(searchTerm, 'suggest')}>
-        이런 곳은 어때요?
-      </span>
+      {/* 장소 추천  */}
+      <div className="flex justify-end items-center font-medium text-[15px]">
+        어디로 가야할지 모르겠다면?
+        <Button
+          type="button"
+          styleType="text"
+          className="ml-2"
+          style={{
+            minWidth: 'auto',
+            fontSize: '15px',
+          }}
+          onClickHandler={() => handleSearchTerm(searchTerm, 'suggest', true)}
+        >
+          장소 추천 받기
+        </Button>
+      </div>
 
       {isNestedSidebar.status === true && (
         <NestedSidebar option={isNestedSidebar} searchTerm={searchTerm} />
