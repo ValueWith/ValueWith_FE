a03b2df3ab22f75038cab7c27499f77bccd15cde
geniee1220 <geniee1220@gmail.com>
2023-11-07 18:57:48 +0900

    Fix : 검색어 갱신됐을 때 기존 데이터가 추가되는 문제 해결

--- src/hooks/useMapSearch.tsx
+++ src/hooks/useMapSearch.tsx
@@ -17,7 +17,14 @@ function useMapSearch({ searchTerm, page = 1 }: MapSearchProps) {
     const placesSearchCB = (data: any, status: any) => {
       if (status === kakao.maps.services.Status.OK) {
         console.log('검색된 데이터', data);
-        setSearchResult([...searchResult, ...data]);
+        console.log(page, '페이지');
+
+        if (page === 1) {
+          setSearchResult(data);
+        } else {
+          setSearchResult([...searchResult, ...data]);
+        }
+
         console.log(searchResult, '다합친 검색 데이터');
 
         // 하나로 쓸 수 있다
