e4bd30a4ee87edf6f91294e0e716dfd88a6d19ea
geniee1220 <geniee1220@gmail.com>
2023-11-07 23:18:35 +0900

    Refactor : 무한 스크롤링 로직을 카드 컴포넌트에서 이동

--- src/components/group/recruit/NestedSidebar/index.tsx
+++ src/components/group/recruit/NestedSidebar/index.tsx
@@ -1,7 +1,8 @@
 import useMapSearch from '@/hooks/useMapSearch';
 import * as S from './NestedSidebar.styles';
-import SearchResultCard from '../SearchResultCard';
-import { useEffect, useState } from 'react';
+import SearchResultCard from '../GroupItemCard';
+import { useEffect, useState, useRef } from 'react';
+import useIntersectionObserver from '@/hooks/useIntersectionObserver';
 
 interface NestedSidebarStatusProps {
   status: boolean;
@@ -15,16 +16,39 @@ interface NestedSidebarProps {
 }
 
 function NestedSidebar({ option, searchTerm }: NestedSidebarProps) {
+  const target = useRef(null);
+
   const [page, setPage] = useState<number>(1);
   const { searchResult } = useMapSearch({ searchTerm, page });
 
-  useEffect(() => {
-    setPage(1);
-  }, [searchTerm]);
+  const [observe, unobserve] = useIntersectionObserver(() => {
+    handlePage();
+  });
 
   const handlePage = () => {
     setPage((page) => page + 1);
   };
+
+  useEffect(() => {
+    const targetRef = target.current;
+    if (!targetRef) return;
+
+    if (target) {
+      observe(targetRef);
+
+      return () => {
+        unobserve(targetRef);
+      };
+    }
+
+    if (searchResult.length === 0) {
+      unobserve(targetRef);
+    }
+  }, [searchResult, target]);
+
+  useEffect(() => {
+    setPage(1);
+  }, [searchTerm]);
 
   return (
     <S.NestedSidebarContainer>
@@ -33,7 +57,17 @@ function NestedSidebar({ option, searchTerm }: NestedSidebarProps) {
           ? '이런 곳은 어때요?'
           : `'${searchTerm}' 검색 결과`}
       </S.NestedSidebarHeading>
-      <SearchResultCard data={searchResult} handlePage={handlePage} />
+
+      <S.GroupItemCardContainer>
+        <ul>
+          {searchResult.map((item: any, index: number) => (
+            <SearchResultCard key={index} item={item} />
+          ))}
+        </ul>
+        {searchResult && searchResult.length > 0 && (
+          <span ref={target} style={{ width: '100%', height: 30 }} />
+        )}
+      </S.GroupItemCardContainer>
     </S.NestedSidebarContainer>
   );
 }
