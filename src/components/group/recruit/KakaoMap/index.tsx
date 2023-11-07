673346e3df4fc5e54f41076cd5f34e8c8218ff36
geniee1220 <geniee1220@gmail.com>
2023-11-07 21:56:29 +0900

    Feat : Marker 적용, Polyline 적용

--- src/components/group/recruit/KakaoMap/index.tsx
+++ src/components/group/recruit/KakaoMap/index.tsx
@@ -1,12 +1,15 @@
+import { selectedPlaceState } from '@/state/GroupRegistState';
 import { useEffect, useState } from 'react';
-import { Map } from 'react-kakao-maps-sdk';
+import { Map, MapMarker, Polyline } from 'react-kakao-maps-sdk';
+import { useRecoilState } from 'recoil';
 
 function KakaoMap() {
+  const [selectedPlaceData] = useRecoilState(selectedPlaceState);
+
   const [mapOptions, setMapOptions] = useState({
     center: { lat: 33.450701, lng: 126.570667 },
     level: 3,
     isPanto: true,
-    zoomable: false,
   });
 
   useEffect(() => {
@@ -36,7 +39,34 @@ function KakaoMap() {
     <Map
       style={{ width: '100%', height: '100%' }} // 지도 크기
       {...mapOptions}
-    ></Map>
+    >
+      {selectedPlaceData.selectedPlace.map((item) => (
+        <MapMarker
+          key={item.id}
+          position={{ lat: item.y, lng: item.x }}
+          image={{
+            src: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png',
+            size: {
+              width: 24,
+              height: 35,
+            },
+          }}
+          title={item.name}
+        />
+      ))}
+
+      {selectedPlaceData.selectedPlace.length >= 2 && (
+        <Polyline
+          path={selectedPlaceData.selectedPlace.map((item) => ({
+            lat: item.y,
+            lng: item.x,
+          }))}
+          strokeColor="#FF0000"
+          strokeOpacity={1}
+          strokeWeight={5}
+        />
+      )}
+    </Map>
   );
 }
 
