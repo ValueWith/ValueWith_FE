bfefc68c38b4b88f1dddc44c787fdf243b9edf60
geniee1220 <geniee1220@gmail.com>
2023-11-07 17:47:16 +0900

    Fix : 검색어 변경 됐을 때 리스트 결과 새로 그리도록 수정

--- src/hooks/useMapSearch.tsx
+++ src/hooks/useMapSearch.tsx
@@ -2,33 +2,46 @@ import { useEffect, useState } from 'react';
 
 const ps = new window.kakao.maps.services.Places();
 
-function useMapSearch({ searchTerm }: { searchTerm: string }) {
+interface MapSearchProps {
+  searchTerm: string;
+  page?: number;
+}
+
+function useMapSearch({ searchTerm, page = 1 }: MapSearchProps) {
   const [searchResult, setSearchResult] = useState<any>([]);
 
   useEffect(() => {
+    if (page > 3) return;
+
     // 장소검색 완료 시 호출
     const placesSearchCB = (data: any, status: any) => {
       if (status === kakao.maps.services.Status.OK) {
         console.log('검색된 데이터', data);
-        setSearchResult(data);
+        setSearchResult([...searchResult, ...data]);
+        console.log(searchResult, '다합친 검색 데이터');
+
+        // 하나로 쓸 수 있다
       } else if (status === kakao.maps.services.Status.ZERO_RESULT) {
-        alert('검색 결과가 존재하지 않습니다.');
-        return;
+        console.log('검색 결과가 존재하지 않습니다.');
+        setSearchResult([]);
       } else if (status === kakao.maps.services.Status.ERROR) {
-        alert('검색 결과 중 오류가 발생했습니다.');
-        return;
+        console.log('검색 결과 중 오류가 발생했습니다.');
+        setSearchResult([]);
       }
     };
 
     // 키워드 검색 요청 함수
     const searchPlaces = () => {
-      ps.keywordSearch(searchTerm, placesSearchCB);
+      ps.keywordSearch(searchTerm, placesSearchCB, {
+        page: page, // 1~3
+        size: 15, // 1~15
+      });
     };
 
     if (searchTerm) {
       searchPlaces();
     }
-  }, [searchTerm]);
+  }, [searchTerm, page]);
 
   return { searchResult };
 }
