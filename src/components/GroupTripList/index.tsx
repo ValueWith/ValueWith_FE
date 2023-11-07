60adffb8315dec6d3f09b2743f557f456a1e4324
jiyujin <yujin1679@naver.com>
2023-11-07 17:51:22 +0900

    Refactor: useSearchParams 로직 변경
    파일위치 - GroupTripList -> GroupMain
    setSearchParams - 파라미터가 변경되었을 때만 수행

--- src/components/GroupTripList/index.tsx
+++ src/components/GroupTripList/index.tsx
@@ -1,20 +1,17 @@
+import { useRecoilState } from 'recoil';
+import ReactPaginate from 'react-paginate';
+
 import useGroupDataFetching from '@/hooks/useGroup';
 import { paramsState } from '@/recoil/paramsState';
-import { useRecoilState } from 'recoil';
 import TripList from '../TripList';
 
 import * as S from './GroupTripList.styles';
-import ReactPaginate from 'react-paginate';
-import { useSearchParams } from 'react-router-dom';
-import { useEffect } from 'react';
 
 interface PageChangeCallback {
   selected: number;
 }
 
 function GroupTripList() {
-  const [searchParams, setSearchParams] = useSearchParams();
-
   const [params, setParams] = useRecoilState(paramsState);
 
   const { data, isLoading, isError } = useGroupDataFetching(params);
@@ -25,23 +22,6 @@ function GroupTripList() {
     const selectedPage = data.selected;
     setParams({ ...params, page: String(selectedPage + 1) });
   };
-
-  useEffect(() => {
-    setParams({
-      ...params,
-      page: searchParams.get('page') || params.page,
-      status: searchParams.get('status') || params.status,
-      area: searchParams.get('area') || params.area,
-      sort: searchParams.get('sort') || params.sort,
-      title: searchParams.get('title') || params.title,
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  useEffect(() => {
-    setSearchParams({ ...params });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [params]);
 
   return (
     <>
--- src/pages/group/GroupMain/index.tsx
+++ src/pages/group/GroupMain/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import GroupSearch from '@/components/GroupSearch';
 import Button from '@/components/Button';
 import GroupFilter from '@/components/GroupFilter';
@@ -8,17 +8,53 @@ import { VscFilter } from 'react-icons/vsc';
 import { PiPencilSimpleLineDuotone } from 'react-icons/pi';
 import { VscListFilter } from 'react-icons/vsc';
 import * as S from './GroupMain.styles';
+import { useRecoilState } from 'recoil';
+import { paramsState } from '@/recoil/paramsState';
+import { useSearchParams } from 'react-router-dom';
+import { GroupListParams } from '@/apis/group';
 
 function GroupMain() {
+  const [params, setParams] = useRecoilState(paramsState);
+  const [searchParams, setSearchParams] = useSearchParams(params);
+
+  const hasDiffParams = () => {
+    return Object.keys(params).some((key) => {
+      const searchParamValue = searchParams.get(key);
+      return searchParamValue !== params[key as keyof GroupListParams];
+    });
+  };
+
+  useEffect(() => {
+    if (hasDiffParams()) {
+      // 초기 파라미터 변경 시(새로고침) setParams
+      setParams({
+        page: searchParams.get('page') || params.page,
+        status: searchParams.get('status') || params.status,
+        area: searchParams.get('area') || params.area,
+        sort: searchParams.get('sort') || params.sort,
+        title: searchParams.get('title') || params.title,
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    if (hasDiffParams()) {
+      // 파라미터 변경 시 setSearchParams로 url 업데이트
+      setSearchParams({ ...params });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [params]);
+
   const [isClickFilter, setIsClickFilter] = useState(false);
   const [isClickSort, setIsClickSort] = useState(false);
 
-  const handleFilter = () => {
+  const handleFilterModal = () => {
     setIsClickFilter(!isClickFilter);
     setIsClickSort(false);
   };
 
-  const handleSort = () => {
+  const handleSortModal = () => {
     setIsClickSort(!isClickSort);
     setIsClickFilter(false);
   };
@@ -35,13 +71,16 @@ function GroupMain() {
       <S.SearchOptionContainer>
         <div className='flex items-center gap-8'>
           <S.FilterButton>
-            <div onClick={handleFilter} className='flex items-center gap-2'>
+            <div
+              onClick={handleFilterModal}
+              className='flex items-center gap-2'
+            >
               필터 <VscFilter />
             </div>
             {isClickFilter && <GroupFilter option={'filter'} />}
           </S.FilterButton>
           <S.FilterButton>
-            <div onClick={handleSort} className='flex items-center gap-2'>
+            <div onClick={handleSortModal} className='flex items-center gap-2'>
               정렬 <VscListFilter />
             </div>
             {isClickSort && <GroupFilter option={'sort'} />}
