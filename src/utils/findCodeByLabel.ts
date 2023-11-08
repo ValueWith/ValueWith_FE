1e094a5e09327d9d981e7eeed45acce2f2af8998
geniee1220 <geniee1220@gmail.com>
2023-11-09 05:54:51 +0900

    Refactor : 라벨로 코드 검색하는 유틸 함수 생성 후 적용

--- src/utils/findCodeByLabel.ts
+++ src/utils/findCodeByLabel.ts
@@ -16,3 +16,20 @@ export const findCodeByLabel = (type: string, label: string, option: any) => {
       return;
   }
 };
+
+export const findLabelByCode = (type: string, code: number, option: any) => {
+  switch (type) {
+    case 'area': {
+      const area = option.find((item: { code: number }) => item.code === code);
+      return area ? area.label : null;
+    }
+    case 'category': {
+      const category = option.find(
+        (item: { code: number }) => item.code === code
+      );
+      return category ? category.label : null;
+    }
+    default:
+      return;
+  }
+};
