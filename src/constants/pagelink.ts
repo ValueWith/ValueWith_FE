7d5507d19263fd66ecc2d44242c23b6eb7801dec
geniee1220 <geniee1220@gmail.com>
2023-11-09 20:33:31 +0900

    Feat : SubMenu Link를 상수로 분리 (#11)

--- src/constants/pagelink.ts
+++ src/constants/pagelink.ts
@@ -8,3 +8,14 @@ export const PAGE_LINK = [
     name: '마이라운지',
   },
 ];
+
+export const MYLOUNGE_SUBMENU_LINK = [
+  {
+    path: '/mylounge/management',
+    name: '그룹 관리',
+  },
+  {
+    path: '/mylounge/profile',
+    name: '내 프로필 수정',
+  },
+];
