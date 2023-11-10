698b7e3896cd7fc6857c3ad5b76929adbfa7078a
geniee1220 <geniee1220@gmail.com>
2023-11-10 23:16:59 +0900

    Refactor : 속성값으로 Value 값 찾도록 공통 유틸 함수 리팩토링

--- src/utils/findCodeByLabel.ts
+++ src/utils/findCodeByLabel.ts
@@ -33,3 +33,16 @@ export const findLabelByCode = (type: string, code: number, option: any) => {
       return;
   }
 };
+
+export const findValueByProperty = (
+  returnType: 'code' | 'label' | 'type',
+  value: string | number,
+  property: 'label' | 'code' | 'type',
+  option: any[]
+): string | number | null => {
+  const item = option.find((item: any) => item[property] === value);
+  return item ? item[returnType] : null;
+};
+
+// const code = findValueByProperty('code', 'Some Label', 'label', option);
+// const label = findValueByProperty('label', 123, 'code', option);
