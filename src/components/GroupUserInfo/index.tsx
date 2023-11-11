8158c43dc97054e03d5176d086a8e4a7f623439c
jiyujin <yujin1679@naver.com>
2023-11-12 02:22:10 +0900

    Refactor: style 추가

--- src/components/GroupUserInfo/index.tsx
+++ src/components/GroupUserInfo/index.tsx
@@ -1,3 +1,4 @@
+import { CSSProperties } from 'react';
 import * as S from './GroupUserInfo.styles';
 
 interface GroupUserInfoProps {
@@ -5,6 +6,7 @@ interface GroupUserInfoProps {
   nickName: string;
   age: number;
   gender: string;
+  style?: CSSProperties;
 }
 
 function GroupUserInfo({
@@ -12,11 +14,12 @@ function GroupUserInfo({
   nickName,
   age,
   gender,
+  style,
 }: GroupUserInfoProps) {
   return (
-    <S.ProfileContainer>
-      <S.ProfileImage src={profileUrl} alt="프로필 이미지" />
-      <span>
+    <S.ProfileContainer style={{ ...style }}>
+      <S.ProfileImage src={profileUrl} alt='프로필 이미지' />
+      <span style={{ ...style }}>
         {nickName}&nbsp;&#183;&nbsp;{age}&nbsp;&#183;&nbsp;{gender}성
       </span>
     </S.ProfileContainer>
