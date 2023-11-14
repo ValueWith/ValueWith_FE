c4e143545e7a22e834ef13999a31876895cb7430
jiyujin <yujin1679@naver.com>
2023-11-14 21:56:47 +0900

    Design: 화면이 800px 이하일 때 1열로 정렬

--- src/pages/chat/Chat.styles.ts
+++ src/pages/chat/Chat.styles.ts
@@ -5,4 +5,8 @@ export const ChatContainer = styled.div`
   display: grid;
   grid-template-columns: 322px 1fr;
   border: 1px solid #e0e0e0;
+
+  @media (max-width: 800px) {
+    grid-template-columns: 1fr; /* 1열로 변경 */
+  }
 `;
