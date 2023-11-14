0e0c1a43e6f22d591e4e196036085d4975b3c439
geniee1220 <geniee1220@gmail.com>
2023-11-14 19:53:48 +0900

    Chore : 프록시 서버 설정

--- vite.config.ts
+++ vite.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://tweaver.site/',
+        target: 'https://tweaver.site',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
         secure: false,
