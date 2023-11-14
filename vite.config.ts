d390c956e2188ba4b37c942d5f85fee650268fb8
geniee1220 <geniee1220@gmail.com>
2023-11-14 19:07:37 +0900

    Chore : 프록시 설정 및 Compression 플러그인 적용

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import path from 'path';
 
 import svgr from 'vite-plugin-svgr';
+import { compression } from 'vite-plugin-compression2';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgr()],
+  plugins: [react(), tsconfigPaths(), svgr(), compression()],
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },
@@ -21,5 +22,16 @@ export default defineConfig({
         replacement: path.resolve(__dirname, 'src/assets'),
       },
     ],
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://tweaver.site/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: false,
+        ws: true,
+      },
+    },
   },
 });
