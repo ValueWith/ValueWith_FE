33168f2c884ea0b6a4c8d6b16263562b975a81e4
geniee1220 <geniee1220@gmail.com>
2023-11-16 02:14:07 +0900

    Refactor : 기존 recoil-persist로 관리하던 accessToken을 localStorage로 수정
    recoil-persist에서는 accessToken의 의존성으로 사용할 state관리를 하도록 수정
    로그인 했을때 데이터 패치에 오류 생기는 현상 확인, token이 필요한 post 요청 이후 다시 리팩토링 예정
    
    # Conflicts:
    #	src/components/Header/index.tsx

--- src/recoil/userState.ts
+++ src/recoil/userState.ts
@@ -3,8 +3,8 @@ import { recoilPersist } from 'recoil-persist';
 
 const { persistAtom } = recoilPersist();
 
-export const tokenState = atom({
-  key: 'tokenState',
-  default: '',
+export const loginState = atom({
+  key: 'loginState',
+  default: false,
   effects_UNSTABLE: [persistAtom],
 });
