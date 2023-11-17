777f56ac305255104aa933eac90785d512183001
geniee1220 <geniee1220@gmail.com>
2023-11-18 01:39:48 +0900

    Design : 수정 및 삭제를 그룹 관리 드롭다운 메뉴로 이동

--- src/components/GroupMemberManagement/index.tsx
+++ src/components/GroupMemberManagement/index.tsx
@@ -1,9 +1,13 @@
-import Button from '../Button';
-
-import { FaChevronDown } from 'react-icons/fa';
-import * as S from './GroupMemberManagement.styles';
 import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import { useGroupMemberList } from '@/hooks/useLounge';
+
+import Button from '../Button';
+
+import { IoArrowForward } from 'react-icons/io5';
+import { IoIosArrowDown } from 'react-icons/io';
+
+import * as S from './GroupMemberManagement.styles';
+import DropdownMenu from '../DropdownMenu';
 
 interface GroupMemberManagementProps {
   tripGroupId: number;
@@ -13,6 +17,17 @@ interface GroupMemberManagementProps {
   >;
   onSetApplyList: (applyList: any) => void;
 }
+
+const DROPDOWN_MENU_OPTIONS = [
+  {
+    label: '수정',
+    onClickHandler: () => console.log('수정'),
+  },
+  {
+    label: '삭제',
+    onClickHandler: () => console.log('삭제'),
+  },
+];
 
 function GroupMemberManagement({
   tripGroupId,
@@ -61,6 +76,25 @@ function GroupMemberManagement({
     <>
       <S.AdminContainer>
         <div className="flex gap-3">
+          {/* 그룹 관리 */}
+          <DropdownMenu options={DROPDOWN_MENU_OPTIONS}>
+            <Button
+              type="button"
+              styleType="basic"
+              size="sm"
+              style={{
+                border: '1px solid #707070',
+                color: '#222222',
+                gap: '4px',
+                height: '31px',
+                padding: '0 8px 0 12px',
+              }}
+            >
+              그룹 관리 <IoIosArrowDown />
+            </Button>
+          </DropdownMenu>
+
+          {/* 멤버 관리 */}
           <Button
             type="button"
             styleType="basic"
@@ -70,12 +104,14 @@ function GroupMemberManagement({
               color: '#222222',
               gap: '4px',
               height: '31px',
-              padding: '12px',
+              padding: '0 8px 0 12px',
             }}
             onClickHandler={(event) => handleClickApplyModal(event, 'approved')}
           >
-            멤버 관리 <FaChevronDown />
+            멤버 관리 <IoIosArrowDown />
           </Button>
+
+          {/* 지원자 목록 보기 */}
           <Button
             type="button"
             styleType="basic"
@@ -85,29 +121,24 @@ function GroupMemberManagement({
               color: '#222222',
               gap: '4px',
               height: '31px',
-              padding: '7px',
+              padding: '0 8px 0 12px',
             }}
             onClickHandler={(event) => handleClickApplyModal(event, 'pending')}
           >
-            지원자 목록 보기 <FaChevronDown />
+            지원자 목록 보기 <IoIosArrowDown />
           </Button>
         </div>
+
         <div className="flex gap-3">
+          {/* 자세히 보기  */}
           <Button
             type="button"
             size="sm"
             styleType="text"
-            style={{ color: '#4e4e4e', minWidth: '0' }}
+            style={{ color: '#000', minWidth: '0' }}
           >
-            수정
-          </Button>
-          <Button
-            type="button"
-            size="sm"
-            styleType="text"
-            style={{ color: '#4e4e4e', minWidth: '0' }}
-          >
-            삭제
+            <span>자세히 보기</span>
+            <IoArrowForward style={{ marginLeft: '5px' }} />
           </Button>
         </div>
       </S.AdminContainer>
