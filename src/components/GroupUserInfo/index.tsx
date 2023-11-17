06603d76cd74bd9ee77b153c1c684588a8f5d96b
geniee1220 <geniee1220@gmail.com>
2023-11-17 21:42:06 +0900

    Design : 멤버 관리인지 지원자 목록인지에 따라 버튼 생성

--- src/components/GroupUserInfo/index.tsx
+++ src/components/GroupUserInfo/index.tsx
@@ -1,7 +1,11 @@
 import { CSSProperties } from 'react';
 import * as S from './GroupUserInfo.styles';
+import Button from '../Button';
+import theme from '@/assets/styles/theme';
+import { conversionGender } from '@/utils/conversionGender';
 
 interface GroupUserInfoProps {
+  type?: string; // approved | pending
   profileUrl: string;
   nickName: string;
   age: number;
@@ -10,6 +14,7 @@ interface GroupUserInfoProps {
 }
 
 function GroupUserInfo({
+  type,
   profileUrl,
   nickName,
   age,
@@ -24,8 +29,60 @@ function GroupUserInfo({
       </S.ProfileImageContainer>
       <S.ProfileInfo>
         <S.ProfileInfoLabel>{age}</S.ProfileInfoLabel>&nbsp;&#183;&nbsp;
-        <S.ProfileInfoLabel>{gender}성</S.ProfileInfoLabel>
+        <S.ProfileInfoLabel>{conversionGender(gender)}</S.ProfileInfoLabel>
       </S.ProfileInfo>
+
+      <div className="flex items-center ml-auto">
+        {type === 'approved' ? (
+          <>
+            <Button
+              type="button"
+              styleType="text"
+              style={{
+                height: '24px',
+                minWidth: 'auto',
+                color: `${theme.color.red300}`,
+                padding: '0 8px',
+                fontSize: '12px',
+              }}
+            >
+              그룹에서 추방
+            </Button>
+          </>
+        ) : (
+          <>
+            <Button
+              type="button"
+              styleType="text"
+              style={{
+                height: '24px',
+                minWidth: 'auto',
+                color: `${theme.color.red300}`,
+                padding: '0 8px',
+                fontSize: '12px',
+              }}
+            >
+              거절
+            </Button>
+            <Button
+              type="button"
+              size="sm"
+              styleType="solid"
+              className="ml-5"
+              style={{
+                height: '24px',
+                minWidth: 'auto',
+                backgroundColor: '#F1F9FF',
+                color: `${theme.color.primary}`,
+                padding: '0 8px',
+                fontSize: '12px',
+              }}
+            >
+              멤버로 초대
+            </Button>
+          </>
+        )}
+      </div>
     </S.ProfileContainer>
   );
 }
