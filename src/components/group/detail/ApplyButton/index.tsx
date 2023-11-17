f2f242a88f5de0a78e10dc8baacb3acaa36a18f9
jiyujin <yujin1679@naver.com>
2023-11-18 04:55:43 +0900

    Feat: 지원하기, 지원취소, 그룹탈퇴 버튼 추가

--- src/components/group/detail/ApplyButton/index.tsx
+++ src/components/group/detail/ApplyButton/index.tsx
@@ -1,18 +1,28 @@
 import { useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { modalState } from '@/recoil/modalState';
+import { QueryClient } from 'react-query';
 
 import { checkApplicationStatus } from '@/utils/checkApplicationStatus';
 import { GroupDetailListItem } from '@/apis/groupDetail';
+import {
+  deleteGroupApply,
+  patchGroupApply,
+  postGroupApply,
+} from '@/apis/groupApply';
 
 import Button from '@/components/Button';
 
 interface ApplyButtonProps {
   data: GroupDetailListItem;
+  queryClient: QueryClient;
 }
 
-function ApplyButton({ data }: ApplyButtonProps) {
+function ApplyButton({ data, queryClient }: ApplyButtonProps) {
   // TODO: 현재 user email 받아와야 함.
   const currentUserEmail = 'loopy@test.com';
   const [applicationStatus, setApplicationStatus] = useState<string>('');
+  const [modalDataState, setModalDataState] = useRecoilState(modalState);
 
   useEffect(() => {
     const status = checkApplicationStatus(
@@ -22,18 +32,102 @@ function ApplyButton({ data }: ApplyButtonProps) {
     );
     setApplicationStatus(status);
   }, [data]);
+
+  const handleApply = () => {
+    setModalDataState({
+      isModalOpen: true,
+      type: 'confirm',
+      title: '지원하기',
+      message: '현재 그룹에 지원 하시겠습니까?',
+      onConfirm: async () => {
+        setModalDataState({ ...modalDataState, isModalOpen: false });
+        await postGroupApply(data.tripGroupDetail.tripGroupId);
+
+        queryClient.invalidateQueries([
+          'groupDetailData',
+          data.tripGroupDetail.tripGroupId,
+        ]);
+      },
+      onCancel: () => {
+        setModalDataState({ ...modalDataState, isModalOpen: false });
+      },
+    });
+  };
+
+  const handleLeaveGroup = () => {
+    setModalDataState({
+      isModalOpen: true,
+      type: 'confirm',
+      title: '그룹 탈퇴하기',
+      message: '현재 그룹에서 탈퇴하시겠습니까?',
+      onConfirm: async () => {
+        setModalDataState({ ...modalDataState, isModalOpen: false });
+        await patchGroupApply(data.tripGroupDetail.tripGroupId);
+
+        queryClient.invalidateQueries([
+          'groupDetailData',
+          data.tripGroupDetail.tripGroupId,
+        ]);
+      },
+      onCancel: () => {
+        setModalDataState({ ...modalDataState, isModalOpen: false });
+      },
+    });
+  };
+
+  const handleCancleApply = () => {
+    setModalDataState({
+      isModalOpen: true,
+      type: 'confirm',
+      title: '지원 취소하기',
+      message: '현재 그룹에 지원을 취소하시겠습니까?',
+      onConfirm: async () => {
+        setModalDataState({ ...modalDataState, isModalOpen: false });
+        await deleteGroupApply(data.tripGroupDetail.tripGroupId);
+
+        queryClient.invalidateQueries([
+          'groupDetailData',
+          data.tripGroupDetail.tripGroupId,
+        ]);
+      },
+      onCancel: () => {
+        setModalDataState({ ...modalDataState, isModalOpen: false });
+      },
+    });
+  };
+
   return (
     <>
-      {applicationStatus &&
-        (applicationStatus === '지원하기' ? (
-          <Button type='button' styleType='solid' fullWidth>
-            지원하기
-          </Button>
-        ) : (
-          <Button type='button' styleType='warning' fullWidth>
-            지원취소
-          </Button>
-        ))}
+      {applicationStatus === '지원하기' && (
+        <Button
+          type='button'
+          styleType='solid'
+          fullWidth
+          onClickHandler={handleApply}
+        >
+          지원하기
+        </Button>
+      )}
+      {applicationStatus === '그룹탈퇴' && (
+        <Button
+          type='button'
+          styleType='warning'
+          fullWidth
+          onClickHandler={handleLeaveGroup}
+        >
+          그룹탈퇴
+        </Button>
+      )}
+      {applicationStatus === '지원취소' && (
+        <Button
+          type='button'
+          styleType='solid'
+          fullWidth
+          onClickHandler={handleCancleApply}
+        >
+          지원취소
+        </Button>
+      )}
     </>
   );
 }
