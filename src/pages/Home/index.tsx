25cafabeef241d623f87130742ce97143b4db344
geniee1220 <geniee1220@gmail.com>
2023-11-19 22:11:55 +0900

    Fix : build 시 이미지를 url로 받아오도록 수정

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,7 +9,12 @@ function Home() {
 
   return (
     <S.HomeMainContainer>
-      <S.Banner />
+      <S.Banner>
+        <img
+          src="https://github.com/ValueWith/ValueWith_FE/assets/110911811/ec0727a0-28d4-43cc-9f1f-d09c000e45a1"
+          alt="배너 이미지"
+        />
+      </S.Banner>
       <S.TitleContainer>
         <S.Title>카테고리 이름</S.Title>
         <S.Link onClick={() => navigate('/group')}>
