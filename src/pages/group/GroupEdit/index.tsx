84d77dbc343425c7157caec303fb43f0e8ddf7f1
geniee1220 <geniee1220@gmail.com>
2023-11-21 00:42:20 +0900

    Refactor : 컨벤션에 맞게 함수 위치 수정

--- src/pages/group/GroupEdit/index.tsx
+++ src/pages/group/GroupEdit/index.tsx
@@ -9,14 +9,13 @@ import KakaoMap from '@/components/group/recruit/KakaoMap';
 import { useEditGroup } from '@/hooks/useRegist';
 
 function GroupEdit() {
+  const { pathname } = useLocation();
+  const { handleEditData } = useEditGroup();
   const [currentStep, setCurrentStep] = useState(1); // Step1 : 기본 정보, Step2 : 일정 선택
 
   const handleFormStep = (step: number) => {
     setCurrentStep(step);
   };
-
-  const { pathname } = useLocation();
-  const { handleEditData } = useEditGroup();
 
   useEffect(() => {
     handleEditData(pathname);
