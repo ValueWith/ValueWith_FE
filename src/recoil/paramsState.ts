23b76246d2e388b056eddd3929d2ce30f210c5ab
geniee1220 <geniee1220@gmail.com>
2023-11-23 02:06:30 +0900

    Refactor : 이미 지정된 타입으로 recoil State 타입 지정(#47)

--- src/recoil/paramsState.ts
+++ src/recoil/paramsState.ts
@@ -1,14 +1,7 @@
+import { GroupListParams } from '@/apis/group';
 import { atom } from 'recoil';
 
-export interface ParamsModel {
-  page: string;
-  status: 'all' | 'open';
-  area: string;
-  sort: 'latest' | 'deadline';
-  title: string;
-}
-
-export const paramsState = atom<ParamsModel>({
+export const paramsState = atom<GroupListParams>({
   key: 'paramsState',
   default: {
     page: '1',
