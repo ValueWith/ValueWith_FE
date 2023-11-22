f51bea5cc6d5817ed8367fd04b9a8bbed41d90e4
jiyujin <yujin1679@naver.com>
2023-11-22 09:16:45 +0900

    Design: 채팅방 목록이 height를 넘어갈 경우 scroll

--- src/components/chat/ChatRoomList/ChatRoomList.styles.ts
+++ src/components/chat/ChatRoomList/ChatRoomList.styles.ts
@@ -2,5 +2,7 @@ import styled from '@emotion/styled';
 
 export const ChatRoomListContainer = styled.div`
   width: 100%;
+  max-height: 830px;
   border-right: 1px solid #e0e0e0;
+  overflow-y: scroll;
 `;
