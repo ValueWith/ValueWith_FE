e9c086c8c9f9f75f48b382bac8a9b77382df62de
jiyujin <yujin1679@naver.com>
2023-11-22 09:16:07 +0900

    Design: 기본 height 설정

--- src/pages/chat/Chat.styles.ts
+++ src/pages/chat/Chat.styles.ts
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 
 export const ChatContainer = styled.div`
   width: 100%;
+  height: 830px;
   display: grid;
   grid-template-columns: 322px 1fr;
   border: 1px solid #e0e0e0;
