eb2b09c9f32ee09579611d752b5535214670c355
jiyujin <yujin1679@naver.com>
2023-11-27 12:22:41 +0900

    Refactor: 채팅 데이터 전역 변수 추가

--- src/recoil/chatRoomState.ts
+++ src/recoil/chatRoomState.ts
@@ -1,28 +1,14 @@
 import { atom } from 'recoil';
-import { Message, RoomInfo } from '@/apis/chat';
+import { RoomInfo } from '@/apis/chat';
 
-const defaultRoomInfo: RoomInfo = {
-  chatRoomId: 0,
-  messages: [],
-  title: '',
-  tripGroupId: 0,
-};
-
-type ChatMessages = {
-  [roomId: number]: Message[];
-};
-
-export const chatRoomState = atom<RoomInfo>({
-  key: 'chatRoomState',
-  default: defaultRoomInfo,
-});
+export type RoomInfoMap = { [chatRoomId: number]: RoomInfo };
 
 export const chatRoomIdState = atom<number>({
   key: 'chatRoomIdState',
   default: 0,
 });
 
-export const chatMessagesState = atom<ChatMessages>({
-  key: 'chatMessagesState',
+export const roomInfoMapState = atom<RoomInfoMap>({
+  key: 'roomInfoMap',
   default: {},
 });
