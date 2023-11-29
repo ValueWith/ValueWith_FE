dfa74b059d6c485d95104d3025dfe6b8584d30a8
geniee1220 <geniee1220@gmail.com>
2023-11-29 19:20:11 +0900

    Fix : 그룹 모집 페이지에서 페이지 로딩 이후, 선택된 데이터가 아무것도 없을 때에만 에러메세지 보이도록 수정
    기본 위치인 경복궁이 일정에 들어갔을 때를 예외처리하기 위함

--- src/components/group/recruit/KakaoMap/index.tsx
+++ src/components/group/recruit/KakaoMap/index.tsx
@@ -1,5 +1,5 @@
 import theme from '@/assets/styles/theme';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import {
   CustomOverlayMap,
   Map,
@@ -25,6 +25,7 @@ function KakaoMap({
   const [selectedPlaceData, setSelectedPlaceData] =
     useRecoilState(selectedPlaceState);
   const [mapOptions, setMapOptions] = useRecoilState(mapOptionState);
+  const [isDefaultLocation, setIsDefaultLocation] = useState<boolean>(false);
 
   // 맵 마커는 selectedPlaceData.selectedPlace에 없을 때만 렌더링
   const shouldRenderMapMarker = selectedPlaceData.selectedPlace.every(
@@ -58,6 +59,7 @@ function KakaoMap({
             });
           },
           (error) => {
+            setIsDefaultLocation(true);
             setMapOptions({
               ...mapOptions,
               center: { lat: 37.577613288258206, lng: 126.97689786832184 },
@@ -133,7 +135,19 @@ function KakaoMap({
 
       {shouldRenderMapMarker &&
         (mapOptions.center.lat !== 0 && mapOptions.center.lng !== 0 ? (
-          <MapMarker position={mapOptions.center}></MapMarker>
+          <MapMarker position={mapOptions.center}>
+            {isDefaultLocation &&
+              selectedPlaceData.selectedPlace.length === 0 && (
+                <div
+                  style={{
+                    padding: '10px',
+                    whiteSpace: 'nowrap',
+                  }}
+                >
+                  현재 위치를 찾을 수 없습니다.
+                </div>
+              )}
+          </MapMarker>
         ) : (
           <>
             <Loader width={50} height={50} className="z-[1]" />
