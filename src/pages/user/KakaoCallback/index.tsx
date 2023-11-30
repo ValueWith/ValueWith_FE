cadc7e22575a6dfff5522d9b569141bff841e40f
geniee1220 <geniee1220@gmail.com>
2023-11-30 20:29:55 +0900

    Refactor : 카카오톡 로그인 로직을 백엔드에서 데이터 처리하도록 변경

--- src/pages/user/KakaoCallback/index.tsx
+++ src/pages/user/KakaoCallback/index.tsx
@@ -1,7 +1,7 @@
 import instance from '@/apis';
-import Loader from '@/components/Loader';
+import Loader from '@/components/common/Loader';
+
 import { modalState } from '@/recoil/modalState';
-import axios from 'axios';
 import { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
@@ -9,35 +9,17 @@ import { useRecoilState } from 'recoil';
 function KakaoCallback() {
   const navigate = useNavigate();
   const location = useLocation();
-  const grantType = 'authorization_code';
-  const code = new URLSearchParams(location.search).get('code');
 
   const [modalDataState, setModalDataState] = useRecoilState(modalState);
 
   const handleKakaoLogin = async () => {
     try {
-      // 카카오로 code를 받아온 후, 해당 code를 이용해 인가 코드를 받아온다.
-      const response = await axios.post(
-        `https://kauth.kakao.com/oauth/token?grant_type=${grantType}&client_id=${
-          import.meta.env.VITE_KAKAO_API_KEY
-        }&redirect_uri=${import.meta.env.VITE_KAKAO_REDIRECT_URI}&code=${code}`,
-        null,
-        {
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
-          },
-        }
+      // 백엔드로 요청을 보내면, 백엔드에서 카카오로부터 받은 인가 코드를 이용해 유저 정보와 jwt 토큰을 받아온다.
+      const response = await instance.post(
+        import.meta.env.VITE_SERVER_URL + '/oauth2/callback/kakao'
       );
 
-      // 백엔드로 카카오로부터 받은 인가 코드를 보내고, 백엔드로부터 유저 정보와 jwt 토큰을 받아온다.
-      const getUserData = await instance.post(``);
-
-      // 받아온 유저 정보를 localStorage에 저장한다.
-      // localStorage.setItem('accessToken', getUserData.accessToken);
-      // localStorage.setItem('userInfo', getUserData.userInfo);
-
-      // 저장하고 나서 메인 페이지로 이동한다.
-      navigate('/');
+      console.log(response, 'response');
     } catch (error) {
       setModalDataState({
         isModalOpen: true,
