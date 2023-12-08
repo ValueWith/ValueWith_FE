b3c43f3151e9e01e2294ef07f19b99278151161a
jiyujin <yujin1679@naver.com>
2023-12-08 16:04:32 +0900

    Feat: 알림 읽음, 삭제 처리 시 즉각적으로 디자인 변경

--- src/components/alarm/AlarmCard/index.tsx
+++ src/components/alarm/AlarmCard/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useQueryClient } from 'react-query';
 
 import { AlarmContent, deleteAlarm, readAlarm } from '@/apis/alarm';
@@ -14,25 +15,43 @@ interface AlarmCardProps {
 function AlarmCard({ data, page }: AlarmCardProps) {
   const queryClient = useQueryClient();
   const createdDate = formatAlarmDate(data.createdDateTime);
+  const [clickDelete, setClickDelete] = useState<boolean>(false);
+  const [clickRead, setClickRead] = useState<boolean>(false);
 
   const handleDelete = async () => {
     await deleteAlarm(data.alertId);
-    queryClient.invalidateQueries(['alarmData', page]);
+    if (page === 0) {
+      queryClient.invalidateQueries(['alarmData', page]);
+    }
+    setClickDelete(true);
   };
 
   const handleRead = async () => {
     await readAlarm(data.alertId);
-    queryClient.invalidateQueries(['alarmData', page]);
+    if (page === 0) {
+      queryClient.invalidateQueries(['alarmData', page]);
+    }
+    setClickRead(true);
   };
 
   return (
-    <S.AlarmCardContainer onClick={handleRead}>
-      <S.AlarmCardContentContainer>
-        <S.AlarmCardContent>
-          <S.AlarmCardGroupName>{data.groupName}</S.AlarmCardGroupName>
+    <S.AlarmCardContainer style={{ display: clickDelete ? 'none' : '' }}>
+      <S.AlarmCardContentContainer onClick={handleRead}>
+        <S.AlarmCardContent
+          className={clickRead || data.isChecked ? 'checked' : ''}
+        >
+          <S.AlarmCardGroupName
+            className={clickRead || data.isChecked ? 'checked' : ''}
+          >
+            {data.groupName}
+          </S.AlarmCardGroupName>
           {data.content.content}
         </S.AlarmCardContent>
-        <S.AlarmCardDate>{createdDate}</S.AlarmCardDate>
+        <S.AlarmCardDate
+          className={clickRead || data.isChecked ? 'checked' : ''}
+        >
+          {createdDate}
+        </S.AlarmCardDate>
       </S.AlarmCardContentContainer>
       <button className='cursor-pointer' onClick={handleDelete}>
         <AiOutlineClose size={13} style={{ color: '#878787' }} />
