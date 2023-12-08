46fe1c8dc344c58542be06a529e0b40b6984ef02
jiyujin <yujin1679@naver.com>
2023-12-08 16:04:04 +0900

    Feat: 새로운 페이지의 데이터 로딩 시 기존 데이터와 합치기

--- src/components/alarm/AlarmModal/index.tsx
+++ src/components/alarm/AlarmModal/index.tsx
@@ -14,6 +14,11 @@ interface AlarmModalProps {
   onClose: () => void;
 }
 
+interface AlarmPageContent {
+  page: number;
+  content: AlarmContent[];
+}
+
 function AlarmModal({ onClose }: AlarmModalProps) {
   const queryClient = useQueryClient();
 
@@ -21,37 +26,55 @@ function AlarmModal({ onClose }: AlarmModalProps) {
 
   const { data, isLoading } = useGetAlarmData(page);
 
-  const [alarmContents, setAlarmContents] = useState<AlarmContent[]>(
-    data ? data.content : []
-  );
+  const [alarmContents, setAlarmContents] = useState<AlarmPageContent[]>([
+    {
+      page: data ? data.pageable.pageNumber : 0,
+      content: data ? data.content : [],
+    },
+  ]);
 
   useEffect(() => {
     if (data) {
       setAlarmContents((prev) => {
-        // prev와 data.content를 JSON 문자열로 변환하여 비교
-        const prevStr = JSON.stringify(prev);
-        const newDataStr = JSON.stringify(data.content);
+        const existingPageDataIndex = prev.findIndex(
+          (item) => item.page === data.pageable.pageNumber
+        );
 
-        // prev와 data.content가 다르면 실행
-        if (prevStr !== newDataStr) {
-          return [...prev, ...data.content];
+        if (existingPageDataIndex !== -1) {
+          // 이미 해당 페이지의 데이터가 있으면 해당 데이터를 갱신
+          const updatedPageData = {
+            ...prev[existingPageDataIndex],
+            content: data.content,
+          };
+
+          // 배열을 복사하고 갱신된 데이터를 적용
+          const updatedData = [...prev];
+          updatedData[existingPageDataIndex] = updatedPageData;
+
+          return updatedData;
+        } else {
+          // 해당 페이지의 데이터가 없으면 새로운 데이터를 추가
+          const newPageData = {
+            page: data.pageable.pageNumber,
+            content: data.content,
+          };
+
+          return [...prev, newPageData];
         }
-
-        // 같으면 이전 상태 그대로 반환
-        return prev;
       });
     }
   }, [data]);
 
   const handlePage = (inView: boolean) => {
-    if (inView && !isLoading && !data?.last) {
-      setPage((page) => page + 1);
+    if (data) {
+      if (inView && !isLoading && !data.last) {
+        setPage((page) => page + 1);
+      }
     }
   };
 
   const { ref } = useInView({
-    triggerOnce: false,
-    threshold: 0.5,
+    threshold: 0.8,
     onChange: handlePage,
   });
 
@@ -70,9 +93,11 @@ function AlarmModal({ onClose }: AlarmModalProps) {
         </S.AlarmModalTitleContainer>
         {isLoading && <Loader />}
         {alarmContents &&
-          alarmContents.map((alarmData: AlarmContent) => (
-            <AlarmCard key={alarmData.alertId} data={alarmData} page={page} />
-          ))}
+          alarmContents.map((alarmData: AlarmPageContent) =>
+            alarmData.content.map((item) => (
+              <AlarmCard key={item.alertId} data={item} page={alarmData.page} />
+            ))
+          )}
         <div ref={ref} />
       </S.AlarmModalContainer>
     </>
