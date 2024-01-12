e10d9d05217ff764982ad7fd6363a876ce8832ef
jiyujin <yujin1679@naver.com>
2024-01-12 17:39:14 +0900

    Refactor: 채팅방 최신그룹순으로 정렬

--- src/components/chat/ChatRoomList/index.tsx
+++ src/components/chat/ChatRoomList/index.tsx
@@ -1,15 +1,15 @@
 import { RoomInfo } from '@/apis/chat';
+import { RoomInfoMap } from '@/recoil/chatRoomIdState';
+
 import ChatRoomCard from '../ChatRoomCard';
 
 import * as S from './ChatRoomList.styles';
-import { RoomInfoMap } from '@/recoil/chatRoomIdState';
 
 interface ChatRoomListProps {
   rooms: RoomInfoMap;
 }
 
 function ChatRoomList({ rooms }: ChatRoomListProps) {
-  console.log('rooms', rooms);
   return (
     <S.ChatRoomListContainer>
       {Object.values(rooms)
