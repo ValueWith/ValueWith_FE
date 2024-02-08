df5efb4ba926c5c4dc772186594a0bac93a57f55
geniee1220 <geniee1220@gmail.com>
2024-02-08 20:08:44 +0900

    Feat : 카카오 로그인 요청 시 유저 정보 localStorage에 담도록 수정

--- src/pages/user/KakaoCallback/index.tsx
+++ src/pages/user/KakaoCallback/index.tsx
@@ -3,34 +3,35 @@ import { tokenRefreshRequest } from '@/apis/user';
 import Loader from '@/components/common/Loader';
 
 import { modalState } from '@/recoil/modalState';
-import { setAccessToken } from '@/utils/localStorage';
+import { loginState } from '@/recoil/userState';
+import { setAccessToken, setUserInfo } from '@/utils/localStorage';
 import { useEffect } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
-import { useRecoilState } from 'recoil';
+import { useRecoilState, useSetRecoilState } from 'recoil';
 
 function KakaoCallback() {
   const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
 
   const [modalDataState, setModalDataState] = useRecoilState(modalState);
+  const setIsLogin = useSetRecoilState(loginState);
 
   const handleKakaoLogin = async () => {
     try {
       // 백엔드로 요청을 보내면, 백엔드에서 유저 정보와 jwt 토큰을 받아온다.
       const refreshToken = searchParams.get('refreshToken');
 
-      // const response = await tokenRefreshRequest();
       const response = await instance.post(
         import.meta.env.VITE_SERVER_URL + '/auth/refresh',
         {
           refreshToken: refreshToken,
-        },
-        {
-          withCredentials: true,
         }
       );
 
       setAccessToken(response.data.accessToken);
+      setUserInfo(response.data.loginMemberIdDto);
+      setIsLogin(true);
+
       navigate('/');
     } catch (error) {
       setModalDataState({
