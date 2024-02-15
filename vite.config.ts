92aa9aca38e134b10495a6a4ccc35a97dd31e69f
geniee1220 <geniee1220@gmail.com>
2024-02-15 10:24:25 +0900

    Chore : Build 관련 패키지 설치 및 번들링 최적화

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite';
+
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import path from 'path';
-
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import svgr from 'vite-plugin-svgr';
 import { compression } from 'vite-plugin-compression2';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgr(), compression()],
+  plugins: [react(), tsconfigPaths(), svgr(), compression(), nodePolyfills()],
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },
@@ -31,6 +32,18 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/, ''),
         secure: false,
         ws: true,
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.indexOf('node_modules') !== -1) {
+            const module = id.split('node_modules/').pop().split('/')[0];
+            return `vendor-${module}`;
+          }
+        },
       },
     },
   },
