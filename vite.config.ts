79a563915e043ce3d2ebf18d94f8f88734274e4d
jiyujin <yujin1679@naver.com>
2024-03-06 17:21:52 +0900

    Refactor: 서버 주소 변경

--- vite.config.ts
+++ vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://tweaver.site',
+        target: 'https://valuewith.site',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
         secure: false,
